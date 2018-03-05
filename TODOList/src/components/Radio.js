d3221771a304521e7dcc4d61f0c51c5c824c8ca1
annaign <anna.a.ignatenkova@yandex.ru>
2018-03-05 10:44:20 +0400

    final update

---
+++ TODOList/src/components/Radio.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Radio(props) {
+  const { id, name, value, defaultChecked, checked, onChange } = props;
+
+  return (
+    <label>
+      <input
+        type="radio"
+        id={id}
+        name={name}
+        value={value}
+        checked={checked}
+        onChange={onChange}
+      />
+      {value}
+    </label>
+  );
+}
