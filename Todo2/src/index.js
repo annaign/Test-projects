d55e10aee0d3c3b9b3c83d26653feccc44398c41
annaign <anna.a.ignatenkova@yandex.ru>
2018-03-05 11:31:53 +0400

    delete

--- Todo2/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TodoApp from './containers/TodoApp';
-import './components/style.css';
-
-ReactDOM.render(<TodoApp />, document.getElementById('todo'));
